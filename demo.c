ca3cb3e0aaa52cb8b2d0fd598e0edd943e2b2e7d
Denys LI <shichangli2020@gmail.com>
2022-10-18 08:41:52 +0800

    two files

--- demo.c
+++ demo.c
@@ -4,6 +4,7 @@ int main(){
     printf("Hello, World!");
 	printf(" change for the first time.");
 	printf("change for the second time");
+	printf("Git has a mutable index called stage.");
     system("pause");
     return 0;
 }
